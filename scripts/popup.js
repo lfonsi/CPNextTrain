de1c152cfe0bea72a35f174c0e5c33fe68bf479c
LuisF <luisf1821@users.noreply.github.com>
2015-06-10 20:09:43 +0100

    Optimisations to get next day trains when necessary

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,10 +1,10 @@
-var origin, destination;
+var origin, destination, trainsToShow = [];
 
 function init() {
     chrome.storage.sync.get('started', function(item) {
         if (item.started) {
             $('#initial-content').remove();
-            getTrain();
+            getTrains(moment());
         } else {
             $('#initial-content').removeClass('hide');
             buildSelects();
@@ -13,7 +13,7 @@ function init() {
     })
 }
 
-function getTrain() {
+function getTrains(date, isNextDay) {
     chrome.storage.sync.get({
             origin: 'Entrecampos',
             destination: 'Benfica',
@@ -24,20 +24,21 @@ function getTrain() {
             destination = items.destination;
             minimumTime = parseInt(items.minimumTime);
 
-            renderStatus('A pesquisar partidas de <b>' + origin + '</b> para <b>' + destination + '</b>');
-            $('.link').removeClass('hide');
+            if (!isNextDay) {
+                renderStatus('A pesquisar partidas de <b>' + origin + '</b> para <b>' + destination + '</b>...');
+                $('.link').removeClass('hide');
+            }
 
             chrome.runtime.sendMessage({
                 type: 'callService',
                 origin: origin,
                 destination: destination,
-                date: moment().format('YYYY-MM-DD')
+                date: date.format('YYYY-MM-DD')
             }, function(response) {
 
                 if (response.success) {
-                    renderStatus(response.msg);
-                    var trains = parseData(response.data);
-                    selectTrains(trains, displayTrains);
+                    var trains = parseData(response.data, date);
+                    selectTrains(trains, displayTrains, date);
                 } else {
                     renderError(response.msg, false);
                 }
@@ -45,7 +46,7 @@ function getTrain() {
         });
 }
 
-function parseData(data) {
+function parseData(data, date) {
     var rows = $(data).find('.table-search-results tbody tr');
     var trains = [];
     $(rows).each(function(index, el) {
@@ -57,8 +58,8 @@ function parseData(data) {
         var duration = cells[4].innerText;
 
         trains.push({
-            departureTime: moment().hour(departureTime[0]).minute(departureTime[1]),
-            arrivalTime: moment().hour(arrivalTime[0]).minute(arrivalTime[1]),
+            departureTime: moment(date).hour(departureTime[0]).minute(departureTime[1]),
+            arrivalTime: moment(date).hour(arrivalTime[0]).minute(arrivalTime[1]),
             duration: duration
         });
     });
@@ -66,38 +67,32 @@ function parseData(data) {
     return trains;
 }
 
-function selectTrains(trains, callback, trainsToShow) {
-    var currentTime = moment().add(minimumTime, 'm'),
-        trainsToShow = [],
+function selectTrains(trains, callback, date) {
+    var currentTime = moment(date).add(minimumTime, 'm'),
         found = false;
 
     chrome.storage.sync.get({
         nrOccurrences: '1',
     }, function(item) {
-        var occurrences = parseInt(item.nrOccurrences);
+        var totalOccurrences = parseInt(item.nrOccurrences);
 
         $.each(trains, function(index, train) {
             if (train.departureTime.isAfter(currentTime)) {
                 trainsToShow.push(train);
-                if (trainsToShow.length === occurrences) {
+                if (trainsToShow.length === totalOccurrences) {
                     found = true;
                     return false;
                 }
             }
         });
 
-        if (!found && trains.length > 0) {
-            var nrTrainsToAdd = (occurrences - trainsToShow.length);
-
-            //builds an array with the missing trains to be processed
-            var toAdd = trains.slice(0, nrTrainsToAdd);
-
-            $.each(toAdd, function(index, train) {
-                train.departureTime.add(1, 'd');
-                trainsToShow.push(train);
-            });
+        if (!found) {
+            getTrains(moment(date).add(1, 'd').startOf('day'), true);
+            return;
         }
+
         callback(trainsToShow);
+
     });
 }
 
@@ -134,8 +129,7 @@ function clickHandler() {
             started: "true",
         }, function() {
             $('#initial-content').remove();
-            $('.link').removeClass('hide');
-            getTrain();
+            getTrains(moment());
         });
     }
 }
